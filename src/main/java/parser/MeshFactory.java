d79d8892819c9640fbe947a06dd50320840865d3
Iwein Bau <iwein.bau@gmail.com>
2020-03-15 12:44:36 +0100

    Stored already loaded meshes in mesh factory.

--- src/main/java/parser/MeshFactory.java
+++ src/main/java/parser/MeshFactory.java
@@ -5,11 +5,15 @@ import geometry.TriangleMesh;
 import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.HashMap;
 
 /**
- * Class for creating new Triangle Meshes.
+ * Class for creating new Triangle Meshes and storing already loaded meshes.
+ *
  */
 public class MeshFactory {
+
+    HashMap<String,TriangleMesh> loadedMeshes = new HashMap<>();
 
     /**
      * Create new triangle mesh from resource file.
@@ -17,11 +21,20 @@ public class MeshFactory {
      * @return TriangleMesh
      */
     public TriangleMesh getTriangleMesh(String fileName) {
+        // Check if the mesh is already loaded?
+        if ( loadedMeshes.containsKey(fileName) ) {
+            return loadedMeshes.get(fileName);
+        }
         // Get file extension
         String extension = fileName.substring(fileName.lastIndexOf("."));
-        if(extension.equals(".obj"))
-            return new OBJLoader(fileName).loadMesh();
-
+        if ( extension.equals(".obj") ) {
+            // Load file
+            TriangleMesh mesh = new OBJLoader(fileName).loadMesh();
+            // Add loaded mesh to already loaded meshes;
+            loadedMeshes.put(fileName,mesh);
+            // Return mesh
+            return mesh;
+        }
         throw new IllegalArgumentException("File extension not supported");
     }
 }
